db4a0e5f842b65d64d52eaeef780094ceea65c9d
CanKadirKarademir <kadircankarademir@hotmail.com>
2020-07-25 17:27:26 +0300

    fix: fix for button component tool

--- components/common/tools/button.js
+++ components/common/tools/button.js
@@ -3,18 +3,19 @@ import { Component } from 'react';
 
 export class Button extends Component {
     render() {
-        return <button type="button" class={'btn btn-block btn-' + this.props.type} disabled={this.props.disabled}>{this.props.name}</button>
+        return <button name={this.props.name} type="button" class={'btn btn-block btn-' + this.props.type} disabled={this.props.disabled}>{this.props.title}</button>
     }
 };
 
 Button.propTypes = {
+    title: PropTypes.string,
     name: PropTypes.string,
     type: PropTypes.string,
     disabled: PropTypes.bool
 };
 
 Button.defaultProps = {
-    name: "Button Name",
+    title: "Button Name",
     type: "success",
     disabled: false
 };
