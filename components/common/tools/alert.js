17904c03a40fd5e56d25d5f1691cfbedbfd71c62
CanKadirKarademir <kadircankarademir@hotmail.com>
2020-07-27 13:52:53 +0300

    feat: added alert component tool

---
+++ components/common/tools/alert.js
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import { Component } from 'react';
+
+export class Alert extends Component {
+    render() {
+        return <div class={"alert alert-dismissible alert-" + this.props.type}>
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+            <h5><i class={"icon fas fa-"+this.props.icon}></i>{this.props.title}</h5>
+            {this.props.children}
+        </div>
+    }
+};
+
+Alert.propTypes = {
+    title: PropTypes.string,
+    type: PropTypes.string,
+    icon: PropTypes.string
+};
+/*
+Danger icon: ban
+Info icon: info
+Warning icon: exclamation-triangle
+Success icon: check
+*/
