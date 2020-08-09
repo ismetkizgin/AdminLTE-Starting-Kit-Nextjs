da20e8d8f1101bd7767cd5129fd160d8abf544e0
ismet kizgin <info@ismetkizgin.com>
2020-08-09 18:06:34 +0300

    fix: fix for admin dashboard

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,11 +1,9 @@
-import { Component } from 'react';
-import { AdminLayout } from '../../components/layout';
-import { Button } from '../../components/common'
+import { Component } from 'react'
+import { AdminLayout } from '../../components/layout'
 
 export default class Index extends Component {
     render() {
         return <AdminLayout contentTitle={'Home'} contentTitleButton={<i className="fa fa-2x fa-home" />} url={this.props.url}>
-            <Button>ismet kizgin</Button>
         </AdminLayout>
     }
 }
