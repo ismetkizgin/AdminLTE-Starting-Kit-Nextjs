e8cdc39e0da86e42a5617881019b19f6f74f4014
Kadir Can KARADEMİR <50851192+CanKadirKarademir@users.noreply.github.com>
2020-08-11 17:00:05 +0300

    fix: fix for client navbar component

--- components/layout/client/navbar/navbar.js
+++ components/layout/client/navbar/navbar.js
@@ -3,40 +3,45 @@ import { Component } from 'react';
 
 export class Navbar extends Component {
     render() {
-        return <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
-            <a class="navbar-brand" href="#">Ailem <b>Güvende</b> LOGO </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+        return <nav className={'navbar navbar-expand-lg navbar-dark bg-' + this.props.type}>
+            <a className="navbar-brand" href="#">{this.props.title}</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">HOME<span className="sr-only">(current)</span></a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#"> ABOUT</a>
                     </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Dropdown    
-                            </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#">Disabled</a>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">CONTACT</a>
                     </li>
                 </ul>
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-                        <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
-          </form>
-        </div>
-      </nav>
+                <form className="form-inline my-2 my-lg-0">
+                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                    <button className={'my-2 my-sm-0 btn btn-outline-' + this.props.searchButtonType} type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
     }
 };
+Navbar.propTypes = {
+    title: PropTypes.string,
+    type: PropTypes.string,
+    searchButtonType: PropTypes.string
+};
+
+Navbar.defaultProps = {
+    title: "Ailem Güvende",
+    type: "dark sticky-top",
+    searchButtonType: "light"
+};
+/*type özelliğine navbar positon verilebilir.
+fixed-top: yukarı sabitleme
+fixed-bottom: aşağı sabitleme
+sticky-top: sayfa akarken yukarı sabitleme
+*/
