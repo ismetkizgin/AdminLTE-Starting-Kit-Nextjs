dc1c38df028d5283157af5a94dfbe44f88be373b
ismet kizgin <info@ismetkizgin.com>
2020-08-14 15:15:02 +0300

    fix: fix for home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { ClientLayout } from '../components/layout'
-import { Navbar } from '../components/layout/client/navbar/navbar'
+
 export default class Index extends React.Component {
     render() {
         return (
