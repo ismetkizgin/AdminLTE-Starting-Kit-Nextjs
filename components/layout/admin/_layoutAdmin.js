f149ed9654c950f34ba830c0c9aae5a37a4afb09
ismet kizgin <info@ismetkizgin.com>
2020-09-03 17:26:44 +0300

    fix: fix for admin layout

--- components/layout/admin/_layoutAdmin.js
+++ components/layout/admin/_layoutAdmin.js
@@ -12,7 +12,6 @@ export class AdminLayout extends Component {
         return <html>
             <Head>
                 <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet" />
-                <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" />
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" />
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/js/all.min.js" integrity="sha512-M+hXwltZ3+0nFQJiVke7pqXY7VdtWW2jVG31zrml+eteTP7im25FdwtLhIBTWkaHRQyPrhO2uy8glLMHZzhFog==" crossOrigin="anonymous"></script>
